7f0959899079524727cca65095cfd7501e1f8986
teng6288 <15665336288@163.com>
2023-06-07 19:05:04 +0800

    one

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <nav>
       <router-link to="/">首页</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">关于</router-link>
     </nav>
     <router-view/>
   </div>
